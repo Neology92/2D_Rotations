d6add6d1d77fc2a5e9c4097b5c667ed59b0817da
Neology92 <oskarlegner@gmail.com>
2019-04-01 01:58:14 +0200

    Drawin in GnuPlot added

--- inc/operating.hh
+++ inc/operating.hh
@@ -2,6 +2,9 @@
 
 #include <iostream>
 #include <string.h>
+#include <fstream>
+
+#include "gnuplot_link.hh"
 #include "Rectangle.hh"
 
 /* 
@@ -22,3 +25,36 @@ void show_options();
  * @param rec - object, which has to bo edited
  */
 void menu(Rectangle& rec);
+
+/**
+ * @brief Draws rectangle in gnuPlot
+ * 
+ */
+int gnuPlotDraw(Rectangle rect);
+
+
+/*!
+ * Saving rectangle coordiantes to file, from which 
+ * GnuPlot program will read, and draw
+ * 
+ * \param[in] filename - filename to write
+ * 
+ * \param[in] R - object of class Rectangle - to save
+ * 
+ * \retval true - success,
+ * \retval false - failed.
+ */
+bool WriteToFileExample(std::string filename, Rectangle R);
+
+
+/*!
+ * Pushing rectangle data (coordinates) to stream
+ * 
+ * \param[in] output_stream - stream to which data will be pushed
+ * 
+ * \param[in] R - object of class Rectangle - to push
+
+ * \retval true - success,
+ * \retval false - failed.
+ */
+void WriteToStreamExample(std::ostream& output_stream, Rectangle R);
