66c66f602d45e55335e0eae6e0fa51b4f9f1ba04
Neology92 <oskarlegner@gmail.com>
2019-04-01 01:19:13 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <string>
 
@@ -15,41 +14,15 @@ using namespace std;
 
 int main()
 {
-
-
-       double verts[4][2]={{1.0,1.0},{11.0,1.0},{11.0,6.0},{1.0,6.0}};
-
        Vector2D V[4];
-
-       for(int i=0; i <POINTS; i++)
-       {
+       double verts[4][2]={{1.0,1.0},{11.0,1.0},{11.0,6.0},{1.0,6.0}};
+       
+       for(int i=0; i <POINTS; i++){
               V[i] = verts[i];
        }
 
        Rectangle R;
-
        R = V;
-       
+
        menu(R);
-
-       return 1;
-
-
-       std::cout << std::fixed << std::setprecision(10) << R << std::endl;
-
-       if(R.check()){
-              std::cout << "The opposite sides are: Equal!" << std::endl;
-       }else{
-              std::cout << "The opposite sides are: Not equal!" << std::endl;
-       }
-       R.len();
-
-       R.rotate(1,3600000);
-
-       if(R.check()){
-              std::cout << "The opposite sides are: Equal!" << std::endl;
-       }else{
-              std::cout << "The opposite sides are: Not equal!" << std::endl;
-       }
-       R.len();
 }
